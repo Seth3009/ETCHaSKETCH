b6bc44e43ccb7471a23a7170abaddc3a2b4f7706
Setiadi <setiadi.seth@gmail.com>
2022-06-03 20:49:10 +0700

    Draw squares inside canvas

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector('.canvas');
+
+// Draw square inside canvas
+function createSquare(){
+  for(let i = 0; i < 256; i++){
+    let div = document.createElement('div');
+    div.classList.add('square');
+    canvas.appendChild(div);
+  }
+}
+
+function updateCanvas() {
+  canvas.innerHTML = "";
+  canvas.style.setProperty(
+    "grid-template-columns",
+    `repeat(${16}, 2fr)`
+  );
+  canvas.style.setProperty(
+    "grid-template-rows",
+    `repeat(${16}, 2fr)`
+  );
+  for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    canvas.appendChild(div);
+  }
+  console.log(16);
+};
+
+createSquare();
